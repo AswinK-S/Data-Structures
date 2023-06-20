db783bfcfd79a1a9b6cd2036de70a127f6f207ef
aswin123456789 <aswinkalathur11@gmail.com.com>
2023-06-21 00:03:35 +0530

    unique

--- unique.js
+++ unique.js
@@ -1,24 +1,23 @@
 function unique(array){
 
     for(let i=0; i<array.length-1; i++){
-        flag=0;
-        for(let j=i+1; j<array.length;j++){
-            if(array[i]===array[j])
+        let flag=0;
+        for(let j=0; j<array.length;j++){
+            if(i!==j &&array[i]===array[j])
             {
                 flag=1
                 break
             }
-        if(flag == 1)
-        {
-           continue
         }
-        else{
+        if(flag === 0)
+        {
             console.log(array[i])
         }
+        
         }
 
     }
 
-}
+let array = [1,2,5,87,6,9,5,2,1]
 
-
+unique(array)
